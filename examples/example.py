d36ddaf7a89a5cb88cb74353c7eb497ab2b47d3d
Lonnie Yu <lonnie.yu@policygenius.com>
2021-02-21 23:48:32 -0500

    Add initial files

---
+++ examples/example.py
@@ -0,0 +1,10 @@
+from ..source.grid import NeighbordingNodes
+
+# initialize 5x5 grid and print grid
+grid = NeighbordingNodes(5, debug=True)
+
+# find coordinates of node in grid
+print(grid.get_node_coordinates(-1))
+print(grid.get_node_coordinates(3))
+print(grid.get_node_coordinates(10))
+print(grid.get_node_coordinates(25))
